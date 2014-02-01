bea320ebfd636cb16e55ab20ddfc701a7d6e4854
git status <rogesson>
2014-02-01 11:27:43 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Recovery::Application.config.secret_token = '404a7f5c880909815eb3e4619881deeec37b80ec4838ee9a560c569e2e81a969ea5ad2b4a85c6ac70eee5bcfcd55a0e68288162928a25b477bc133bce02ee8a4'
