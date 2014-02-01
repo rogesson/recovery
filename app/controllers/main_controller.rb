6c19b78627aa2ace7669960dc67707a00e119528
git status <rogesson>
2014-02-01 12:26:50 +0000

    Save a new user with validation of password

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,18 @@
 class MainController < ApplicationController
 	def index
+
+	end
+
+	def create
+		
+		if params[:password_conf] == params[:password]
+			User.create(
+				:login => params[:login],
+			 	:password => params[:password],
+			 	:email => params[:email]
+			 	)
+		else
+			redirect_to :back			
+		end
 	end
 end
