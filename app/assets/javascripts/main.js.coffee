921e6ce4545431d1a5fd49842982e7d38fb5d132
git status <rogesson>
2014-02-08 17:56:55 +0000

    Create form for login and add submit javascript

--- app/assets/javascripts/main.js.coffee
+++ app/assets/javascripts/main.js.coffee
@@ -1,3 +1,12 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+
+function requestTo(){
+	var xdp = { name: $("#weq")[0].value, location: "Boston" }
+	$.ajax({
+	  type: "GET",
+	  url: "main/1",
+	  data: xdp
+	})
+}
--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,10 +1,10 @@
 class MainController < ApplicationController
+	require 'json'
 	def index
 
 	end
 
 	def create
-		
 		if params[:password_conf] == params[:password]
 			User.create(
 				:login => params[:login],
@@ -13,6 +13,12 @@ class MainController < ApplicationController
 			 	)
 		else
 			redirect_to :back			
+		end
+	end
+
+	def show
+		respond_to do |format|
+			format.json {render :json => "test"}
 		end
 	end
 end
