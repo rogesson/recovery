49a2f07583e7ddd984e8b13f76aed6bbe5016bdc
git status <rogesson>
2014-02-15 23:33:42 +0000

    update password

--- app/controllers/credential_controller.rb
+++ app/controllers/credential_controller.rb
@@ -2,7 +2,11 @@ class CredentialController < ApplicationController
 	require 'json'
 	#PUT credential/:id
 	def update
-		params[:password]
+		
+		credential = Credential.find(params[:id])
+		credential.password = params[:password]
+		credential.save
+
 		respond_to do |format|
   			format.json { render :json => "test" }
 		end
