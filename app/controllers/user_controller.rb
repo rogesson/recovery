3265f773fdcadca75c83b45412083859def2b0c0
git status <rogesson>
2014-02-16 00:10:54 +0000

    Create method of search

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,13 @@
 class UserController < ActionController::Base
   def show
-  	@user = User.find(params["id"].to_i)
-  	@passwords = Credential.find_all_by_user_id(@user.id)
+  	if params[:search].nil?
+  	 @user = User.find(params["id"].to_i)
+  	 @passwords = Credential.find_all_by_user_id(@user.id)
+  	
+  	elsif params[:type] == "1"
+  		#type 1 is search by site name ;D
+  		@passwords = Credential.find_by_sql "select * from credentials where user_id = #{params["id"]} and site like '%#{params[:search]}%'" 
+  	end
+
   end
 end
