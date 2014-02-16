277ede44c7b6875a9bcb8e369f24a36c75506aa9
git status <rogesson>
2014-02-16 01:15:31 +0000

    Search completed

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,22 +1,20 @@
 class UserController < ActionController::Base
-  def show
-  		
-  	#if params[:search].nil?
-  	 #@user = User.find(params["id"].to_i)
-  	 #@passwords = Credential.find_all_by_user_id(params["id"].to_i)
-  	if params[:type] == "1"
-  		#type 1 is search by site name ;D
-  		@passwords = Credential.find_by_sql "select * from credentials where user_id = #{params["id"]} and site like '%#{params[:search]}%'" 
-  	elsif params[:type] == "2"
-  		#type 1 is search by login
-  		@passwords = Credential.find_by_sql "select * from credentials where user_id = #{params["id"]} and login like '%#{params[:search]}%'" 
-  	elsif params[:type] == "3"
-  		#type 1 is search by password
-  		#needed?
-  		@passwords = Credential.find_by_sql "select * from credentials where user_id = #{params["id"]} and password like '%#{params[:search]}%'"
-  	else
-  		 @passwords = Credential.find_all_by_user_id(params["id"].to_i)
-  	end
+  
+	def show
+	  	if params[:type] == "1"
+	  		#type 1 is search by site name ;D
+	  		@passwords = Credential.find_by_sql "select * from credentials where user_id = #{params["id"]} and site like '%#{params[:search]}%'" 
+	  	elsif params[:type] == "2"
+	  		#type 1 is search by login
+	  		@passwords = Credential.find_by_sql "select * from credentials where user_id = #{params["id"]} and login like '%#{params[:search]}%'" 
+	  	elsif params[:type] == "3"
+	  		#type 1 is search by password
+	  		#needed?
+	  		@passwords = Credential.find_by_sql "select * from credentials where user_id = #{params["id"]} and password like '%#{params[:search]}%'"
+	  	else
+	  		#search all
+	  		@passwords = Credential.find_all_by_user_id(params["id"].to_i)
+	  	end
+	end
 
-  end
 end
