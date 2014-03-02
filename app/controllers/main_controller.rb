08af4dca26731b015aa2786fca271ff9e309e482
git status <rogesson>
2014-03-02 15:57:08 +0000

    Change MD5 to SHA256 and add SHA256 login too

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -9,7 +9,7 @@ class MainController < ApplicationController
 		if params[:password_conf] == params[:password]
 			User.create(
 				:login => params[:login],
-			 	:password => Digest::MD5.hexdigest(params[:password]),
+			 	:password => Digest::SHA256.hexdigest(params[:password]),
 			 	:email => params[:email]
 			 	)
 		else
@@ -18,14 +18,12 @@ class MainController < ApplicationController
 	end
 
 	def home_page
-	
-		user = User.where(:login => params[:login], :password => params[:password])
+		user = User.where(:login => params[:login], :password => Digest::SHA256.hexdigest(params[:password]))
 		if  user != []
 			redirect_to "/user/#{user[0].id}"
 		else
 			redirect_to '/main?bad_login=1'
 		end
-
 	end
 
 	def new_password
