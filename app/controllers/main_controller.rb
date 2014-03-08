496ad4fdbeba7e35a6d34205471f6fee762cd80b
git status <rogesson>
2014-03-08 20:14:01 +0000

    Create session module

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -20,6 +20,10 @@ class MainController < ApplicationController
 	def home_page
 		user = User.where(:login => params[:login], :password => Digest::SHA256.hexdigest(params[:password]))
 		if  user != []
+
+			session[:session_id] = Random.rand(19999283)
+			session[:user_id] = user[0].id
+
 			redirect_to "/user/#{user[0].id}"
 		else
 			redirect_to '/main?bad_login=1'
--- app/models/credential.rb
+++ app/models/credential.rb
@@ -2,4 +2,15 @@ class Credential < ActiveRecord::Base
 	self.table_name = "credentials"
 
 	attr_accessible :login, :password, :site, :user_id
+
+	def encrypt
+		@salt = "123"
+		Digest::SHA1.hexdigest("--#{@salt}--#{self.password}--")
+		e = Encode.new("abc")
+		pas1 = e.encrypt
+	end
+	
+	def decript
+
+	end
 end
