79aaf917f0d0222b1773259c23795b65b6d0839c
git status <rogesson>
2014-04-18 04:20:18 +0100

    More testes

--- test/functional/credential_controller_test.rb
+++ test/functional/credential_controller_test.rb
@@ -1,7 +1,17 @@
 require File.expand_path("../../test_helper", __FILE__)
 
 class CredentialControllerTest < ActionController::TestCase
+	setup do
+		@controller = CredentialController.new
+	end
+
 	test "the truth" do
     	assert true
+	end
+
+	test "should update credential" do
+		put(:update, id: 56)
+		#puts Credential.last.password
+		#puts params[:password]
 	end
 end
