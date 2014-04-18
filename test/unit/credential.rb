d2b6817bf158e8adf3dfe8df46cd92cfdc3a4500
git status <rogesson>
2014-04-18 18:51:20 +0100

    Create password change test

--- test/unit/credential.rb
+++ test/unit/credential.rb
@@ -1,7 +1,35 @@
 require File.expand_path("../../test_helper", __FILE__)
 
 class CredentialTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
-   end
+	setup do
+	#session[:user_id] = 5
+
+	end
+
+   
+   	test "the truth" do
+    	assert true
+   	end
+
+   	test "should change password" do
+   		
+   		status = false
+   		id = 56
+   		credential = Credential.find(id).taint
+
+   		password = "npass"
+   		
+
+   		if credential.user_id == 5
+   			status = false
+   			credential.untaint
+   			if credential.tainted? == false
+   				status = true
+	   			credential.password = password
+	   			credential.save
+   			end
+   		end
+
+   		puts status
+   	end
 end
