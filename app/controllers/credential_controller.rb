e0f4f0c8e625d760813574ffc946b8b024f267d0
git status <rogesson>
2014-05-10 23:02:00 +0100

    Fix credential response

--- app/controllers/credential_controller.rb
+++ app/controllers/credential_controller.rb
@@ -1,25 +1,29 @@
 class CredentialController < ApplicationController
-	
+	require 'json'
 	#PUT credential/:id
 	def update
 		credential = Credential.find(params[:id]).taint
 
 		if credential.change_password params[:password]
-			@response = {:response => 200}
+			@response = "Password Changed!"
 		else
-			@response = {:response => 500}
+			@response = "Password not changed, try again."
 		end
 
 		
-		#render :json => @response.to_json
-
-		respond_to do |format|
- 			format.json { render :json => "qweqwe" }
-		end
+		render :js => "alert('#{@response}');"
 	end
 
 	#DELETE credential/:id
 	def destroy
-		credential = Credential.find(params[:id]).delete
+		
+		if Credential.find(params[:id]).delete
+			@response = "Deleted!"
+		else
+			@response = "Try Again"
+		end
+
+		
+		render :js => "alert('#{@response}'); window.location.reload();"
 	end
 end
