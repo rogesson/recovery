03211c0e51d80339a7542d26c8af5cf00d9756c8
git status <rogesson>
2014-05-11 00:17:51 +0100

    Fix search route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,14 +18,38 @@ class UsersController < ApplicationController
 	def show
 	  	if params[:type] == "1"
 	  		#type 1 is search by site name ;D
-	  		@passwords = Credential.find_by_sql "select * from credentials where user_id = #{session[:user_id]} and site like '%#{params[:search]}%'" 
+	  		@passwords = Credential.find_by_sql "
+	  		SELECT * FROM
+	  			credentials
+	  		WHERE 
+	  			user_id = #{session[:user_id]}
+	  		AND 
+	  			site 
+	  		LIKE 
+	  			'%#{params[:search]}%'" 
 	  	elsif params[:type] == "2"
 	  		#type 1 is search by login
-	  		@passwords = Credential.find_by_sql "select * from credentials where user_id = #{session[:user_id]} and login like '%#{params[:search]}%'" 
+	  		@passwords = Credential.find_by_sql "
+	  		SELECT * FROM 
+	  			credentials 
+	  		WHERE 
+	  			user_id = #{session[:user_id]}
+	  		AND 
+	  			login
+	  		LIKE 
+	  			'%#{params[:search]}%'" 
 	  	elsif params[:type] == "3"
 	  		#type 1 is search by password
 	  		#needed?
-	  		@passwords = Credential.find_by_sql "select * from credentials where user_id = #{session[:user_id]} and password like '%#{params[:search]}%'"
+	  		@passwords = Credential.find_by_sql "
+	  		SELECT * FROM 
+	  			credentials 
+	  		WHERE 
+	  			user_id = #{session[:user_id]}
+	  		AND 
+	  			password 
+	  		LIKE 
+	  			'%#{params[:search]}%'"
 	  	else
 	  		#search all
 	  		@passwords = Credential.find_all_by_user_id(session[:user_id])
