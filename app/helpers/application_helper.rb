f47eebac1e537218131ebbc3b020b3f22ef60988
git status <rogesson>
2014-06-14 20:17:01 +0100

    create ApplicationHelper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def self.navi_item
+		arr = Array.new
+		navi_item = arr.push({href: "/home", value: "Home"})
+		navi_item = arr.push({href: "/credentials", value: "Credential"})	
+		navi_item = arr.push({href: "/sessions/logout", value: "logout"})
+
+		navi_item
+	end
 end
