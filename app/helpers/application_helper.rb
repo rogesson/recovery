1b984114ecb7dd1b1abd25cb9bf8594916ed8c85
git status <rogesson>
2014-06-21 04:20:35 +0100

    create notes nav

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
 	def self.nav_item
 		arr = Array.new
 		nav_itemx = arr.push({href: "/home", value: "Home"})
-		nav_itemx = arr.push({href: "/credentials", value: "Credential"})	
+		nav_itemx = arr.push({href: "/credentials", value: "Credentials"})	
+		nav_itemx = arr.push({href: "/notes", value: "Notes"})
 		nav_itemx = arr.push({href: "/sessions/logout", value: "Logout"})
 
 		nav_itemx
