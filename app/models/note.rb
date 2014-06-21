01042948da3f4d943d82da3ba5eedc63ed911a6e
git status <rogesson>
2014-06-21 18:56:37 +0100

    short_title

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,13 @@ class Note < ActiveRecord::Base
    self.table_name = "notes"
 
    attr_accessible :title, :body, :user_id
+
+   def short_title
+   	if self.title.size < 10
+   		"#{self.title}".capitalize[0..9]
+   	else
+   		"#{self.title.capitalize[0..9]}..."
+   	end 
+
+   end
 end
