d4f29f539d90c27d07caab5554f2092f051e485e
git status <rogesson>
2014-06-25 01:14:07 +0100

    fix session skip

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
-	skip_before_filter :verify_active_session
+	skip_before_filter :verify_active_session, only: :index
 
 	def index
 		render layout: false
