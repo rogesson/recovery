3a125f222090ff14b0b59331e5599ce6455bd1da
git status <rogesson>
2014-06-29 18:51:09 +0100

    count messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
-	def self.nav_item
+	def self.nav_item user_id
 		arr = Array.new
 		nav_itemx = arr.push({href: "/home", value: "Home"})
 		nav_itemx = arr.push({href: "/credentials", value: "Credentials"})	
 		nav_itemx = arr.push({href: "/notes", value: "Notes"})
 		nav_itemx = arr.push({href: "/users", value: "All Users"})
-		nav_itemx = arr.push({href: "/messages", value: "Messages"})
+		nav_itemx = arr.push({href: "/messages", value: "Messages (#{User.find(user_id).unread_message_count})"})
 		nav_itemx = arr.push({href: "/sessions/logout", value: "Logout"})
 
 		nav_itemx
