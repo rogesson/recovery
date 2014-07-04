32660da97299f76d1fdb4d79e099ef38177a9ad0
git status <rogesson>
2014-07-04 17:06:41 +0100

    user edit controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,10 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def edit
+		@user = User.find(session[:user_id])
+	end
+
 	def index
 		@users = User.where("id != ?", session[:user_id])
 	end  
