533e294a50ecb719f0744f2227d7d7b0ec565831
rogesson <rogessonb@gmail.com>
2015-06-20 19:36:08 +0100

    comentarios

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -2,11 +2,10 @@ class Credential < ActiveRecord::Base
 	self.table_name = "credentials"
 
 	belongs_to :user
-
+	# TODO Refatorar
 	validates :login,    presence: true 
 	validates :password, presence: true 
 	validates :site,     presence: true 
-
 
 	attr_accessible :login, :password, :site, :user_id
 	
@@ -49,7 +48,7 @@ class Credential < ActiveRecord::Base
 		else
 			""
 		end
-		
+
 		self.where("#{column} LIKE ?", "%#{params[:search]}%")
 	end
 end
