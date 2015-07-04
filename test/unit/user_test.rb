dde05a5a6a9df8967d6ebe173e8a4508e7e89080
rogesson <rogessonb@gmail.com>
2015-07-04 17:06:01 +0100

    novo teste

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,6 +3,13 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   test "should create User" do
     user = User.new(login: 'user_login', password: 'password', email: 'email')
-    assert user.valid?
+
+    assert user.save
+  end
+
+  test "should update User" do
+    user = users(:rogesson)
+
+    assert user.update_attributes(login: 'log1', password: 'pas1', email: 'mai1')
   end
 end
