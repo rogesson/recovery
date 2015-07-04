851a1f41d1edef131013e3c4027d62b27eb8beb5
rogesson <rogessonb@gmail.com>
2015-07-04 17:10:56 +0100

    find test

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -12,4 +12,10 @@ class UserTest < ActiveSupport::TestCase
 
     assert user.update_attributes(login: 'log1', password: 'pas1', email: 'mai1')
   end
+
+  test "should get user" do
+    user_id = users(:rogesson).id
+
+    assert_nothing_raised { User.find(user_id) }
+  end
 end
