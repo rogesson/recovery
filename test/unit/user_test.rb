e3c2aa47d25c7a1c02a7e179f29268680a8aed37
rogesson <rogessonb@gmail.com>
2015-07-04 17:17:30 +0100

    teste destroy

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,21 +1,28 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "should create User" do
+  test 'should create User' do
     user = User.new(login: 'user_login', password: 'password', email: 'email')
 
     assert user.save
   end
 
-  test "should update User" do
+  test 'should update User' do
     user = users(:rogesson)
 
     assert user.update_attributes(login: 'log1', password: 'pas1', email: 'mai1')
   end
 
-  test "should get user" do
+  test 'should get User' do
     user_id = users(:rogesson).id
 
     assert_nothing_raised { User.find(user_id) }
+  end
+
+  test 'should destroy User' do
+    user = users(:rogesson)
+    user.destroy
+
+    assert_raise(ActiveRecord::RecordNotFound) { User.find(user.id) }
   end
 end
