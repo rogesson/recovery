8bfe3b73966f2f8bdf41b0b0ef9af9f59c76022b
rogesson <rogessonb@gmail.com>
2015-07-11 04:21:11 +0100

    ajuste nos forms de login

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,8 @@ class MainController < ApplicationController
 	skip_before_filter :verify_active_session, only: :index
 
 	def index
+    @user = User.new
+    @session = Session.new
 		render layout: false
 	end
 
