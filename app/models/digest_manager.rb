d7e775784a4647159c5ef108de053855cd2df1cb
rogesson <rogessonb@gmail.com>
2015-07-11 20:08:43 +0100

    ajuste de no desc

--- app/models/digest_manager.rb
+++ app/models/digest_manager.rb
@@ -1,10 +1,12 @@
 class DigestManager
 
 	def self.enc(content, c_key)
+    #
 		Gibberish::AES.new(c_key).encrypt(content)
 	end
 
 	def self.dec(content, c_key)
+    #
 		Gibberish::AES.new(c_key).decrypt(content)
 	end
 end
