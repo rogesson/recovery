14848ab1839455aaf16349660186c4581d679332
= <rogessonb@gmail.com>
2015-10-09 04:23:00 +0100

    novos testes para categoria

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,10 @@
 class Category < ActiveRecord::Base
   self.table_name = "categories"
 
+  attr_accessible :title
+
+  validates :title, presence: true
+  
   def created_at_formatted
     created_at.strftime("%d/%m/%Y")
   end
--- test/unit/category_test.rb
+++ test/unit/category_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+
+  test 'should create category' do
+    assert Category.create(title: 'Music')
+  end
+
+  test 'should update category' do
+    assert category.update_attributes(title: 'Series')
+  end
+
+  test 'should destroy category' do
+    category.destroy
+
+    assert_raise(ActiveRecord::RecordNotFound) { Category.find(category.id) }
+  end
+
+  test 'should validate attributes' do
+    new_category = Category.new
+
+    assert !new_category.valid?
+    assert new_category.errors[:title].any?
+
+    assert_equal ["can't be blank"], new_category.errors[:title]
+  end
+
+  private
+
+  def category
+    categories(:study)
+  end
 end
